5c2fa5cbeabe062b338796838656416f269de826
Ytronix <abdulkadersadim89@gmail.com>
2025-10-02 20:08:19 +0600

    Complete Home Animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
-import './App.css'
-import './Fonts/anurati-webfont/style.css';
-import './Fonts/quick-webfont/stylesheet.css'
-import BgVideo from './Components/BgVideo';
-import Navbar from './Components/Navbar';
-import Hero from './Components/Hero';
+import "./App.css";
+import "./Fonts/anurati-webfont/style.css";
+import "./Fonts/quick-webfont/stylesheet.css";
+import BgVideo from "./Components/BgVideo";
+import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
+import { useState } from "react";
+import About from "./Components/About";
 
 function App() {
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState("home");
 
   return (
     <>
       <BgVideo></BgVideo>
-      
-      <Navbar></Navbar>
+
+      <Navbar
+        active={active}
+        setActive={setActive}
+        setToggle={setToggle}
+      ></Navbar>
 
       <main>
-        <Hero></Hero>
+        {toggle === "home" && <Hero></Hero>}
+        {toggle === "projects" && <p>Projects</p>}
+        {toggle === "skills" && <p>Skills</p>}
+        {toggle === "about" && <p>About</p>}
+        {toggle === "contact" && <About></About>}
       </main>
     </>
   );
 }
 
-export default App
+export default App;
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,34 +4,64 @@ import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Hero = () => {
   return (
-    <div>
-      <img
-        src={Myphoto}
-        alt=""
-        className="w-[600px] absolute -z-10 top-35 lg:top-auto lg:bottom-0 lg:right-20"
-      />
-      <div className="info mt-126 lg:mt-60 ml-8 lg:ml-20">
-        <h1 className="info_name anurati uppercase text-[36px] lg:text-6xl drop-shadow drop-shadow-[#727fde] mb-8">
-          abdul kader bhuiyan
-        </h1>
-        <div className="info-title w-48 h-11 relative flex justify-center items-center rounded-3xl overflow-hidden mb-8 -ml-2.5">
-          <p className="flex items-center justify-center absolute z-10 quick font-bold bg-[#121212] w-46.5 h-9.5 text-center rounded-4xl">
-            web developer
-          </p>
-        </div>
-        <div className=" info-links flex gap-10 items-center ml-1.5 lg:ml-0">
-          <a href="#home">
-            <FaFacebook className="info_link relative text-3xl"></FaFacebook>
-          </a>
-          <a href="#github">
-            <FaGithub className="info_link relative text-3xl"></FaGithub>
-          </a>
-          <a href="#linkedin">
-            <FaLinkedin className="info_link relative text-3xl"></FaLinkedin>
-          </a>
+    <section className="hero">
+      <div className="hidden animate__animated animate__fadeInDown lg:flex flex-row-reverse justify-between mt-8 overflow-hidden">
+        <img
+          src={Myphoto}
+          alt=""
+          className="animate__animated animate__fadeInTopLeft w-[600px] top-35 lg:top-auto lg:bottom-0 lg:right-20 "
+        />
+        <div className="info animate__animated animate__fadeInTopRight lg:ml-20 lg:mt-60 pl-10 lg:pl-0">
+          <h1 className="info_name anurati uppercase text-[36px] lg:text-[55px] drop-shadow drop-shadow-[#727fde] mb-8">
+            abdul kader bhuiyan
+          </h1>
+          <div className="info-title w-48 h-11 relative flex justify-center items-center rounded-3xl overflow-hidden mb-8 -ml-2.5">
+            <p className="flex items-center justify-center absolute z-10 quick font-bold bg-[#0f0325] w-46.5 h-9.5 text-center rounded-4xl">
+              web developer
+            </p>
+          </div>
+          <div className=" info-links flex gap-10 items-center ml-1.5 lg:ml-0">
+            <a href="#home">
+              <FaFacebook className="info_link relative text-3xl"></FaFacebook>
+            </a>
+            <a href="#github">
+              <FaGithub className="info_link relative text-3xl"></FaGithub>
+            </a>
+            <a href="#linkedin">
+              <FaLinkedin className="info_link relative text-3xl"></FaLinkedin>
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="block h-[93vh] py-20 animate__animated animate__fadeInDown lg:hidden overflow-hidden">
+        <img
+          src={Myphoto}
+          alt=""
+          className="w-[600px] top-35 lg:top-auto lg:bottom-0 lg:right-20 "
+        />
+        <div className="info lg:ml-20 lg:mt-60 pl-10 lg:pl-0">
+          <h1 className="info_name anurati uppercase text-[36px] lg:text-[55px] drop-shadow drop-shadow-[#727fde] mb-8">
+            abdul kader bhuiyan
+          </h1>
+          <div className="info-title w-48 h-11 relative flex justify-center items-center rounded-3xl overflow-hidden mb-8 -ml-2.5">
+            <p className="flex items-center justify-center absolute z-10 quick font-bold bg-[#0f0325] w-46.5 h-9.5 text-center rounded-4xl">
+              web developer
+            </p>
+          </div>
+          <div className=" info-links flex gap-10 items-center ml-1.5 lg:ml-0">
+            <a href="#home">
+              <FaFacebook className="info_link relative text-3xl"></FaFacebook>
+            </a>
+            <a href="#github">
+              <FaGithub className="info_link relative text-3xl"></FaGithub>
+            </a>
+            <a href="#linkedin">
+              <FaLinkedin className="info_link relative text-3xl"></FaLinkedin>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
